df33645217fe1a490461f2fb6e8a3447c026774f
Randy Leighton <randyleighton@yahoo.com>
2014-08-11 06:52:11 +0000

    add error checking, code working

--- lib/roman_convert.rb
+++ lib/roman_convert.rb
@@ -1,32 +1,25 @@
-@numbers_to_convert = [["M"  , 1000],
-	                    ["CM" , 900],
-	                    ["D"  , 500],
-	                    ["CD" , 400],
-                      ["C"  , 100],
-                      ["XC" ,  90],
-	                    ["L"  ,  50],
-	                    ["XL" ,  40],
-	                    ["X"  ,  10],
-	                    ["IX" ,   9],
-	                    ["V"  ,   5],
-	                    ["IV" ,   4],
-	                    ["I"  ,   1]]
+@roman_values = [[1000, "M"], [900,  "CM"], [500, "D"], [400, "CD"], [100, "C"], [90, "XC"],
+                 [50, "L"], [40, "XL"], [10, "X"], [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]]
+@menu_counter = 0
 
 def main_menu
   loop do
+  system("clear")
+  if @menu_counter > 0
+    puts "Last conversion Answer:  #{@number} is equal to #{@final_answer}\n\n"
+  end
     puts "Choose an option:"
     puts "[1] convert a number to a roman numeral"
-    puts "[2] convert a roman numeral to a number"
+    puts "[2] Exit program\n\n"
     menu_choice = gets.chomp
     if menu_choice == "1"
-      print "Enter integer (ie. 12, 123, 3890) to convert: "
-      number_entered = gets.chomp
-
+      print "Enter integer (less than 3999) to convert:"
+      num_entered = gets.chomp.to_i
+      if num_entered <= 3999
+        @number = num_entered
+        to_roman(num_entered)
+      end
     elsif menu_choice == "2"
-      puts "Enter Roman numberal (ie. MCM, IV, MXCIV) to convert: "
-      number_entered = gets.chomp
-
-    elsif menu_choice == "3"
       puts "Done."
       exit
     else 
@@ -35,12 +28,17 @@ def main_menu
   end
 end
 
-def to_Roman(int_number)
-  
-end
-
-def to_integer(rom_number)
-
+def to_roman(int_number)
+  @menu_counter += 1
+  int_num = int_number.to_i
+  answer = []
+  @roman_values.each do |key, value|
+    while int_num >= key
+      answer << value
+      int_num -= key
+    end  
+  end
+  @final_answer = answer.join
 end
 
 main_menu
